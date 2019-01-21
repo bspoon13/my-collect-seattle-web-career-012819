5e1029ca5caf0853982e59ce78755a1c52dc5a1b
Billy Witherspoon <wwither@gmail.com>
2019-01-21 04:34:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(array)
   i = 0
   while i < array.length
-    array[i] = yield(array[i])
+    newArray = []
+    newArray << yield(array[i])
     i += 1
   end
-  array
+  newArray
 end
